beb46397c0b09311932d792701c482d595f0f73c
luru <luru@eventbrite.com>
2022-07-26 17:12:35 +0200

    Remove old code

--- src/gilded_rose/gilded_rose.py
+++ src/gilded_rose/gilded_rose.py
@@ -3,20 +3,6 @@ class GildedRose(object):
         self.items = items
 
     def update_quality(self):
-        for i in range(0, len(self.items)):
-            if "Normal" == self.items[i].name:
-                self.items[i].update_quality()
-                return
-
-            if "Aged Brie" == self.items[i].name:
-                self.items[i].update_quality()
-                return
-
-            if "Backstage passes to a TAFKAL80ETC concert" == self.items[i].name:
-                self.items[i].update_quality()
-                return
-
-            if "Sulfuras, Hand of Ragnaros" == self.items[0].name:
-                self.items[i].update_quality()
-                return
+        for item in self.items:
+            item.update_quality()
         return self.items
